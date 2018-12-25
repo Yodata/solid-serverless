7231e8a697ad68f28920700c62f07502b225f62e
Dave Duran <dave@yodata.io>
2018-12-24 22:40:12 -0800

    remove .aws-sam from source-control

--- deploy/aws/component/data-processing/.aws-sam/build/checkScope/data-processing.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = dataProcessing;
-
-function dataProcessing() {
-    // TODO
-}
--- deploy/aws/component/data-processing/.aws-sam/build/createView/index.js
+++
@@ -1,2 +0,0 @@
-exports.checkScope = require('./check-scope').handler
-exports.createView = require('./create-view').handler
