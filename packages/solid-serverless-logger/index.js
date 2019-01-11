1a3533ea66e90e8e876b075693d044f1b3db5824
Dave Duran <dave@yodata.io>
2019-01-11 13:11:02 -0800

    feat(logger now exports .formats & .transports from winston for more customization options):

--- packages/solid-serverless-logger/index.js
+++ packages/solid-serverless-logger/index.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 const winston = require('winston')
 
 const format = winston.format.combine(
@@ -8,9 +10,26 @@ const transports = [
 	new winston.transports.Console()
 ]
 
-const level = process.env.DEBUG_LEVEL || process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+const defaultLogLevel = () => {
+	let level
+	switch (process.env.NODE_ENV) {
+	case 'development':
+		level = 'debug'
+		break
+	case 'production':
+		level = 'info'
+		break
+	default:
+		level = 'info'
+	}
+	return level
+}
+
+const level = process.env.DEBUG_LEVEL || defaultLogLevel()
 
 const defaultOptions = {format, transports, level}
 
 exports.defaultLogger = winston.createLogger(defaultOptions)
 exports.createLogger = options => winston.createLogger(Object.assign({},defaultOptions,options))
+exports.format = winston.format
+exports.transports = winston.transports
