f76c570f589ccd5d6831d605d3df5b1cb0b42e30
Max Dor <git@max.dorius.io>
2019-04-16 01:33:55 +0200

    Add profile topic event generator

---
+++ service/profile-processor/src/main/java/App.java
@@ -0,0 +1,108 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import io.yodata.GsonUtil;
+import io.yodata.ldp.solid.server.aws.handler.container.ContainerHandler;
+import io.yodata.ldp.solid.server.aws.store.S3Store;
+import io.yodata.ldp.solid.server.model.*;
+import io.yodata.ldp.solid.server.model.Event.StorageAction;
+import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.time.Instant;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class App implements RequestStreamHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    private ContainerHandler storeHandler;
+
+    public App() {
+        storeHandler = new ContainerHandler(S3Store.getDefault());
+    }
+
+    @Override
+    public void handleRequest(InputStream input, OutputStream output, Context context) throws IOException {
+        String raw = IOUtils.toString(input, UTF_8);
+        try {
+            handleRequest(GsonUtil.parseObj(raw));
+        } catch (JsonSyntaxException | IllegalStateException | IllegalArgumentException e) {
+            log.error("Invalid JSON object received - Data: {}", raw, e);
+        }
+    }
+
+    private void handleRequest(JsonObject obj) {
+        if (!obj.has("Records")) { // This is not from SNS/SQS
+            log.info("This is a regular message");
+            process(obj);
+        } else {
+            log.info("Processing as wrapped messages");
+            JsonArray records = obj.getAsJsonArray("Records");
+            records.forEach(recordEl -> {
+                JsonObject record = recordEl.getAsJsonObject();
+                if (record.has("Sns")) {
+                    String dataRaw = record.get("Sns").getAsJsonObject().get("Message").getAsString();
+                    log.info("SNS data: {}", dataRaw);
+                    process(GsonUtil.parseObj(dataRaw));
+                } else if (record.has("body")) {
+                    String body = record.getAsJsonPrimitive("body").getAsString();
+                    log.info("SQS data: {}", body);
+                    process(GsonUtil.parseObj(body));
+                } else {
+                    throw new IllegalArgumentException("This is not a SNS or SQS message, cannot process");
+                }
+            });
+        }
+    }
+
+    private void process(JsonObject obj) {
+        StorageAction event = GsonUtil.get().fromJson(obj, StorageAction.class);
+        log.info("Processing event {}", event.getId());
+        URI objId = event.getRequest().getTarget().getId();
+        String podId = objId.resolve("/profile/card#me").toString();
+
+        JsonObject actionNew = new JsonObject();
+        actionNew.addProperty(ActionPropertyKey.Type.getId(), event.getType());
+        actionNew.addProperty(ActionPropertyKey.Timestamp.getId(), event.getRequest().getTimestamp().toEpochMilli());
+        actionNew.addProperty(ActionPropertyKey.Instrument.getId(), event.getRequest().getSecurity().getInstrument());
+        event.getRequest().getSecurity().getAgent().ifPresent(a -> actionNew.addProperty(ActionPropertyKey.Agent.getId(), a));
+        if (event.getObject().isPresent()) {
+            actionNew.add(ActionPropertyKey.Object.getId(), event.getObject().get());
+        } else {
+            actionNew.addProperty(ActionPropertyKey.Object.getId(), event.getId());
+        }
+
+        JsonObject notification = new JsonObject();
+        notification.addProperty("topic", "profile");
+        notification.addProperty(ActionPropertyKey.Type.getId(), "Notification");
+        notification.addProperty(ActionPropertyKey.Timestamp.getId(), Instant.now().toEpochMilli());
+        notification.addProperty(ActionPropertyKey.Instrument.getId(), podId);
+        notification.add(ActionPropertyKey.Object.getId(), actionNew);
+
+        SecurityContext sc = new SecurityContext();
+        sc.setAgent(podId);
+        sc.setInstrument(podId);
+        sc.setAdmin(true);
+
+        Target target = Target.forPath(new Target(objId), "/event/topic/profile/");
+        Request r = new Request();
+        r.setMethod("POST");
+        r.setTarget(target);
+        r.setSecurity(sc);
+        r.setBody(notification);
+
+        Response res = storeHandler.post(r);
+        String eventId = GsonUtil.parseObj(res.getBody().get()).get("id").getAsString();
+        log.info("Topic event was saved at {}", eventId);
+    }
+
+}
