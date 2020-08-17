af2b4bd937442bf0ac4ea33c390a36cf52e93f8f
Max Dor <git@max.dorius.io>
2020-08-17 13:21:04 +0200

    Use new config for transform service

--- packages/solid-server-aws-java-sdk/src/main/java/io/yodata/ldp/solid/server/aws/transform/AWSTransformService.java
+++ packages/solid-server-aws-java-sdk/src/main/java/io/yodata/ldp/solid/server/aws/transform/AWSTransformService.java
@@ -5,21 +5,19 @@ import com.amazonaws.services.lambda.AWSLambdaClientBuilder;
 import com.amazonaws.services.lambda.model.InvokeRequest;
 import com.amazonaws.services.lambda.model.InvokeResult;
 import com.google.gson.JsonObject;
-import io.yodata.EnvUtils;
 import io.yodata.GsonUtil;
+import io.yodata.ldp.solid.server.aws.Configs;
 import io.yodata.ldp.solid.server.model.transform.TransformMessage;
 import io.yodata.ldp.solid.server.model.transform.TransformService;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AWSTransformService implements TransformService {
 
-    private AWSLambda lambda;
-    private String lName;
+    private final AWSLambda lambda;
+    private final String lName;
 
     public AWSTransformService() {
-        lName = EnvUtils.find("TRANSFORM_AWS_LAMBDA_NAME").orElse("create-view");
+        lName = Configs.get().get("aws.lambda.transform.name");
         lambda = AWSLambdaClientBuilder.defaultClient();
     }
 
