e8a5b8ecd7fedab8f444e37d5fb87d0fb029bce6
Dave Duran <dave@yodata.io>
2020-10-18 22:59:55 -0700

    svc/validate-schema can fetch remote schema by url

--- service/validate-schema/src/__tests__/test-handler.js
+++ service/validate-schema/src/__tests__/test-handler.js
@@ -2,12 +2,17 @@ const handler = require('..').handler
 const event = require('../example/event.json')
 const response = require('../example/response.json')
 
-const context = {}
+// test('verifies successful response', async () => {
+// 	const result = await handler(event)
+// 	return expect(result).toMatchObject(response)
+// })
 
-describe('Tests index', () => {
-	test('verifies successful response', async () => {
-		const result = await handler(event, context)
-		return expect(result).toMatchObject(response)
-	})
+test('remote schema', async () => {
+	const object = {
+		topic: 'realestate/thisbetternotwork'
+	}
+	const schema = 'https://raw.githubusercontent.com/Yodata/real-estate/master/www/schemas/franchise/franchise.transactionreport.yaml'
+	const event = { object, schema }
+	const result = await handler(event)
+	return expect(result).toHaveProperty('isValid', false)
 })
-
