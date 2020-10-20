3ec2126d439ec4f34f7176e2e7f79763ac810ad5
Dave Duran <dave@yodata.io>
2020-10-20 12:50:26 -0700

    create-sfdc-contact stub

---
+++ service/create-sfdc-contact/README.md
@@ -0,0 +1,69 @@
+# lambda-service
+
+what does it do?
+
+```bash
+.
+├── README.md
+├── event.json
+├── response.json
+├── script
+│   ├── create-bucket
+│   ├── deploy
+│   └── package
+├── src
+│   ├── __tests__
+│   │   └── unit
+│   │       └── test-handler.js
+│   ├── index.js
+│   └── package.json
+└── template.yaml
+```
+
+## Requirements
+
+* AWS CLI already configured with Administrator permission
+* [NodeJS 8.10+ installed](https://nodejs.org/en/download/)
+* [Docker installed](https://www.docker.com/community-edition)
+
+## Setup process
+
+### Building the project
+
+```bash
+. script/build
+```
+
+By default, this command writes built artifacts to `.aws-sam/build` folder.
+
+### Local development
+
+```bash
+. script/invoke
+```
+
+## Packaging and deployment
+
+If you need to create the bucket for this package...
+
+```bash
+. script/create-bucket
+```
+
+Run the following command to package our Lambda function to S3:
+
+```bash
+. script/package
+```
+
+Next, the following command will create a Cloudformation Stack and deploy your SAM resources.
+
+```bash
+. script/deploy
+```
+
+## Testing
+
+```bash
+jest
+```
---
+++ service/create-sfdc-contact/src/index.js
@@ -0,0 +1,29 @@
+const logger = require('@yodata/logger')
+
+/**
+ * What does your function do?
+ * @param {object} event
+ * @param {string} event.param - comment
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
+ * @param {Object}   context
+ * @param {string}   context.logGroupName - Cloudwatch Log Group name
+ * @param {string}   context.logStreamName - Cloudwatch Log stream name.
+ * @param {string}   context.functionName - Lambda function name.
+ * @param {string}   context.memoryLimitInMB - Function memory.
+ * @param {string}   context.functionVersion - Function version identifier.
+ * @param {function} context.getRemainingTimeInMillis - Time in milliseconds before function times out.
+ * @param {string}   context.awsRequestId - Lambda request ID.
+ * @param {string}   context.invokedFunctionArn - Function ARN.
+ *
+ * @returns {object} response -
+ */
+exports.handler = async (event, context) => {
+	try {
+		logger.debug('create-sfdc-contact received event', {event, context})
+	} catch (error) {
+		logger.error('create-sfdc-contact failed', {error, context})
+	}
+	logger.debug('create-sfdc-contact response', event)
+	return event
+}
