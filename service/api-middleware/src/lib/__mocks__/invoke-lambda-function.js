1ddb6d778b0791047c997773f8ef49197adb20ce
Dave Duran <dave@yodata.io>
2020-10-30 19:01:04 -0700

    api-middleware: add-tests

--- service/api-middleware/src/lib/__mocks__/invoke-lambda-function.js
+++ service/api-middleware/src/lib/__mocks__/invoke-lambda-function.js
@@ -1,5 +1,3 @@
-// @ts-check
-const assert = require('assert-plus')
 
 /**
  * Normalizes a lambda invocation to work like a regular async call
@@ -9,18 +7,12 @@ const assert = require('assert-plus')
  * @returns {Promise<object>} returns the InvokeResponse.Payload
  */
 const invokeLambdaFunction = async (FunctionName, event, lambdaConfig = {}) => {
-  assert.string(FunctionName)
-  assert.object(event)
-  assert.optionalObject(lambdaConfig)
-  const { error, response } = lambdaConfig
-  if (error) {
-    throw new Error(error)
-  }
-  return response ? response : event
+	const { error, response } = lambdaConfig
+	if (error) {
+		throw new Error(error)
+	}
+	return response ? response : event
 }
 
-const functionError = response => {
-  return (typeof response.FunctionError !== 'undefined')
-}
 
 module.exports = invokeLambdaFunction
